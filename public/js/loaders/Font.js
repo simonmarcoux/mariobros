5a683c8c17f059e4b220707a02b807a346f40e2a
simonmarcoux <sim_marc@hotmail.com>
2018-08-27 21:36:12 -0400

    Loop in font sprite

--- public/js/loaders/Font.js
+++ public/js/loaders/Font.js
@@ -1,14 +1,31 @@
 import { loadImage } from "../Loaders";
 import SpriteSheet from "../SpriteSheet";
 
-const 
+const CHARS = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~';
+
 export function loadFont() {
     return loadImage('./assets/img/font.png')
         .then(image => {
             const fontSprite = new SpriteSheet(image);
 
-            fontSprite.define('A', 8, 16, 8, 8);
+            const size = 8; 
+            const rowLen = image.width;
+
+            // convert iterable to array and loop in the new entries 
+            for (let [index, char] of [...CHARS].entries()) {
+               const x = index * size % rowLen;
+               const y = Math.floor(index * size / rowLen) * size;
+               fontSprite.define(char, x, y, size, size);
+            //    console.log(index, char, x, y); 
+            }
+
 
             return fontSprite;
         });
+}
+
+class Font {
+    constructor() {
+        
+    }
 }
